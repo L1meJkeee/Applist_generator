de703aa8d8e3597f2f704b5fe8af86cb90cd6815
L1meJkeee <39944938+L1meJkeee@users.noreply.github.com>
2022-04-19 02:45:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ PATH = os.getcwd() + "\\"
 
 def open_file():
     file_types = (('Text files', '*.txt'),
-                  ('All files', '*.*'))
+                  ('All files', '*.*')
+                 )
 
     filename = fd.askopenfilename(
         title='Выберите файл с AppID',
         initialdir=os.getcwd(),
-        filetypes=file_types)
+        filetypes=file_types
+    )
     if filename != '':
         file_name = filename.split('/')
         output = f'Файл "{file_name[-1]}" успешно загружен и Applist сгенерирован'
@@ -39,26 +41,26 @@ root = Tk()
 root.title('AppList generator')
 root.iconbitmap(PATH+'gui\\generator.ico')
 root.resizable(width=False, height=False)
+image = Image.open(f"{PATH}gui\\bg_image.jpg").resize((850, 600))
+background_image = ImageTk.PhotoImage(image)
 
 canvas = Canvas(root, width=300, height=200, bg='white')
 canvas.pack(fill="both", expand=True)
-
-image = Image.open(f"{PATH}gui\\bg_image.jpg").resize((850, 600))
-background_image = ImageTk.PhotoImage(image)
 canvas.create_image(-100, -100, image=background_image)
-
 canvas.create_text(150, 50,
                    text="Выберите файл с AppID\n"
                         "программа сгенерирует Applist",
                    fill='white',
                    justify=CENTER,
-                   font=('Arial', 12))
+                   font=('Arial', 12)
+                  )
 
 open_button = Button(root,
                      text='Сгенерировать папку Applist',
                      font=('Arial', 12),
                      activebackground='gray',
-                     command=open_file)
+                     command=open_file
+                    )
 open_button.place(relx=0.13, rely=0.65)
 
 root.mainloop()
